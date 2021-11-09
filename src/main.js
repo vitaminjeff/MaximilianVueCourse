10dd637fb3b2476940f76077849a2eede5cc71d3
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-08 16:41:08 -0800

    Flesh out the BaseButton component, register it in main.js and use it

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router.js';
 import store from './store/index.js';
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 
 const app = createApp(App)
 
@@ -11,5 +12,6 @@ app.use(router);
 app.use(store);
 
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
