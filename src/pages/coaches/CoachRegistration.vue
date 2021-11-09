1e301ea2ceb0be5dd2fc2257120ed6f4b8d81e55
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-09 14:21:54 -0800

    Add CoachForm component, import and use it in CoachRegistration component

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,6 +1,18 @@
 <template>
-   <section>
+  <section>
+    <base-card>
       <h2>Register as a coach now!</h2>
-      
-   </section>
+      <coach-form> </coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
