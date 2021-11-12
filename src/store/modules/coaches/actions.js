39f21b367a90abdb8f2283ed6b5013309ca68bd0
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-12 13:01:47 -0800

    Add mutation setCoaches, add action loadCoaches, call action from CoachesList component method in click handler and created() lifecycle hook

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,5 +26,31 @@ export default {
          ...coachData,
          id: userId
       });
+   },
+   async loadCoaches(context) {
+      const response = await fetch(`${process.env.VUE_APP_API_URL}/coaches.json`);
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+         // error ...
+      }
+
+      const coaches = [];
+
+      for (const key in responseData) {
+         const coach = {
+            id: key,
+            firstName: responseData[key].firstName,
+            lastName: responseData[key].lastName,
+            description: responseData[key].description,
+            hourlyRate: responseData[key].hourlyRate,
+            areas: responseData[key].areas,
+         };
+
+         coaches.push(coach);
+      }
+
+      context.commit('setCoaches', coaches);
    }
 };
