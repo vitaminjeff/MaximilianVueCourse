1325ba3bb8effd426c0db5e1bad1a57b9d8dd695
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-12 18:05:39 -0800

    Add /auth route, implement UserAuth component, add authModule to vuex, move state and getters into authModule, setup empty actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,15 @@
 import { createStore } from 'vuex';
 
-import coachesModule from './modules/coaches/index.js'
-import requestsModule from './modules/requests/index.js'
+import coachesModule from './modules/coaches/index.js';
+import requestsModule from './modules/requests/index.js';
+import authModule from './modules/auth/index.js'; // not namespaced
 
 const store = createStore({
    modules: {
       coaches: coachesModule,
-      requests: requestsModule
+      requests: requestsModule,
+      auth: authModule,
    },
-   state() {
-      return {
-         userId: 'c3' // dummy id for now
-      }
-   },
-   getters: {
-      userId(state) {
-         return state.userId;
-      }
-   }
 });
 
 export default store;
