33fe5450741954e425e289d2325b7c3854e77816
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-12 23:10:41 -0800

    Add login() action and dispatch it from the submitForm() method of the UserAuth component

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -71,15 +71,17 @@ export default {
 
       this.isLoading = true;
 
+      const actionPayload = {
+        email: this.email,
+        password: this.password,
+      };
+
       try {
         // send http request
         if (this.mode === 'login') {
-          // ...
+          await this.$store.dispatch('login', actionPayload);
         } else {
-          await this.$store.dispatch('signup', {
-            email: this.email,
-            password: this.password,
-          });
+          await this.$store.dispatch('signup', actionPayload);
         }
       } catch(err) {
         this.error = err.message || 'Failed to authenticate, try later.'
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,8 +1,36 @@
 export default {
-   login() {},
+   async login(context, payload) {
+      // https://firebase.google.com/docs/reference/rest/auth
+      const uri = `${process.env.VUE_APP_FIREBASE_ACCOUNTS_URL}:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_API_KEY}`
+      console.log(uri);
+
+      const response = await fetch(uri, {
+         method: 'POST',
+         body: JSON.stringify({
+            email: payload.email,
+            password: payload.password,
+            returnSecureToken: true
+         })
+      });
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+         console.log(responseData)
+         const error = new Error(responseData.message || 'Failed to authenticate. Check your login data.');
+         throw error;
+      }
+
+      console.log(responseData);
+      context.commit('setUser', {
+         token: responseData.idToken,
+         userId: responseData.localId,
+         tokenExpiration: responseData.expiresIn
+      });
+   },
    async signup(context, payload) {
-      
-      const uri = `${process.env.VUE_APP_AUTH_SIGNUP_URL}?key=${process.env.VUE_APP_FIREBASE_API_KEY}`
+      // https://firebase.google.com/docs/reference/rest/auth
+      const uri = `${process.env.VUE_APP_FIREBASE_ACCOUNTS_URL}:signUp?key=${process.env.VUE_APP_FIREBASE_API_KEY}`
       console.log(uri);
 
       const response = await fetch(uri, {
